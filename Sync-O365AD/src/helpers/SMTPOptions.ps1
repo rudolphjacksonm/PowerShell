ea7161b84d93ce785b7fdd15e796c9bede713397
Jack Morris <rudolphjacksonm@gmail.com>
2016-07-16 09:18:38 +0100

    Create SMTPOptions.ps1

---
+++ Sync-O365AD/src/helpers/SMTPOptions.ps1
@@ -0,0 +1,5 @@
+# SMTP Settings
+$Global:smtpserver = "IP Address of SMTP Server"
+$Global:emailalertFrom = 'examplefromaddress@github.com'
+$Global:emailalertTo = 'example_mailbox@github.com'
+$Global:emailsubject = 'O365 Sync'
