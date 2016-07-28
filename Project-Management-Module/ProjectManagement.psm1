5d41641a38c995075988481333a210e0699d3a04
Jack Morris <rudolphjacksonm@gmail.com>
2016-07-28 16:14:17 +0100

    Updated Compile-Project Function
    Reworked Compile-Project to copy everything to a file in a folder called _flatscript instead of _amp
    Changed New-ProjectTree to create same folder structure but renamed _amp folder to _flatscript

--- Project-Management-Module/ProjectManagement.psm1
+++ Project-Management-Module/ProjectManagement.psm1
@@ -1,4 +1,6 @@
-﻿# Moves src code to stable
+# Moves src code to stable and excludes Pester unit testing file
+# Further code (to be added) will run unit tests and, if 
+# successful, continue with copy the code to stable.
 
 function Copy-SrcToStable {
     [CmdletBinding()]
@@ -19,11 +21,11 @@ function Copy-SrcToStable {
         # Get src directory
         $SourceDir = Get-ChildItem 'src'
         
-        # Flat files at root of directory
+        # Flat files at root of directory; exclude Pester test file when moving to stable
         $SourceDir | Where-Object {$_.psiscontainer -eq $false -and $_.Name -notlike '*Tests.ps1'} | select -ExpandProperty FullName | Copy-Item -Destination Stable -Force
 
         # Subfolders, resursively copied
-        $sourcedir | Where-Object {$_.psiscontainer -eq $true -and $_.Name -notlike '*_amp*'} | Select -exp FullName | Copy-Item -Recurse -Destination stable -Force
+        $sourcedir | Where-Object {$_.psiscontainer -eq $true -and $_.Name -notlike '*_flatscript*'} | Select -exp FullName | Copy-Item -Recurse -Destination stable -Force
 
     }
     else {
@@ -68,7 +70,7 @@ function New-ProjectTree {
                 New-Item -ItemType Directory ($Path + '\src')
                 New-Item -ItemType Directory ($Path + '\stable')
                 New-Item -ItemType Directory ($Path + '\src\helpers')
-                New-Item -ItemType Directory ($Path + '\_amp')
+                New-Item -ItemType Directory ($Path + '\_flatscript')
 
             }
             catch {
@@ -85,7 +87,7 @@ function New-ProjectTree {
                     New-Item -ItemType Directory ($Path + '\src')
                     New-Item -ItemType Directory ($Path + '\stable')
                     New-Item -ItemType Directory ($Path + '\src\helpers')
-                    New-Item -ItemType Directory ($Path + '\_amp')
+                    New-Item -ItemType Directory ($Path + '\_flatscript')
 
                 }
                 catch {
@@ -108,7 +110,7 @@ function New-ProjectTree {
             New-Item -ItemType Directory $Name\src
             New-Item -ItemType Directory $Name\stable
             New-Item -ItemType Directory $Name\src\helpers
-            New-Item -ItemType Directory $Name\_amp
+            New-Item -ItemType Directory $Name\_flatscript
 
         }
         else {
@@ -116,13 +118,13 @@ function New-ProjectTree {
             New-Item -ItemType Directory src
             New-Item -ItemType Directory stable
             New-Item -ItemType Directory src\helpers
-            New-Item -ItemType Directory _amp
+            New-Item -ItemType Directory _flatscript
 
         }
     }
 }
 
-# Flatten project directory in to runscript and drop in _amp folder
+# Flatten project directory in to runscript and drop in _flatscript folder
 
 function Compile-Project {
     [CmdletBinding()]
@@ -140,20 +142,19 @@ function Compile-Project {
         Write-Output "Switch to a project directory and run Compile-Project."
 
     }
-    
 
-    $parameters = Get-ScriptParameters -FullName $mainScriptFile.Fullname
+	$parameters = Get-ScriptParameters -FullName $mainScriptFile.Fullname
 
-    $scriptBody = Get-ScriptBody -FullName $mainScriptFile.Fullname
+	$scriptBody = Get-ScriptBody -FullName $mainScriptFile.Fullname
 
-    $runScriptFile = @()
-    $runScriptFile += $parameters
-    $runScriptFile += ' '
-    $runScriptFile += $scriptBody
+	$flatScriptFile = @()
+	$flatScriptFile += $parameters 
+	$flatScriptFile += ' ' 
+	$flatScriptFile += $scriptBody
 
-    # Output the final script
-    $runScriptOutput = "$pwd\_amp\$($mainScriptFile.Name)" -replace '(.*).ps1','$1_runScript.ps1'
-    $runScriptFile | Out-File $runScriptOutput
+	# Output the final script
+	$flatScriptOutput = "$pwd\_flatscript\$($mainScriptFile.Name)" -replace '(.*).ps1','$1_flatScript.ps1'
+	$flatScriptFile | Out-File $flatScriptOutput
 
 }
 
@@ -197,19 +198,14 @@ function Get-ScriptBody {
 
     $script = @()
     $scriptBody |  ForEach-Object { 
-        if($_ -notmatch '[.]\s[.]'){
+        if($_ -notmatch 'Import[-]Module\s'){
             $script += $_
 
         }
         else{
-            $nestedScript = Get-Content ($_ -replace '[.] [.]\\','stable\\').trimstart()
+            $nestedScript = Get-Content ($_ -replace 'Import[-]Module\s[$]global[:]ScriptRoot\\','stable\')
 
             $nestedScript |  ForEach-Object { 
-                if($_ -notmatch '[.]\s[.]'){
-                    $script += $_
-
-                }
-                else{
                     $nestedScript = Get-Content ($_ -replace '[.] [.]\\','stable\\').trimstart()
 
                     $script += $nestedScript
@@ -218,6 +214,3 @@ function Get-ScriptBody {
             }
         }
     }
-
-    $script
-}
